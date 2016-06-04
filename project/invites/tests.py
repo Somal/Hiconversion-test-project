18cbe2ce39b39b026b881e8d8c6dd13244fc8323
somal <somal1996@rambler.ru>
2016-06-04 23:34:33 +0300

    Add tests

--- project/invites/tests.py
+++ project/invites/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.test.client import Client
+from models import *
 
-# Create your tests here.
+
+class Testing(TestCase):
+    def test_allInviteRequestsIsSatisfied(self):
+        invite_users = Invites.objects.exclude(email__in=User.objects.values_list('email'))
+        self.assertEqual(invite_users.count(), 0)
+
+    def test_allEmailsAreDifferent(self):
+        flag = True
+        try:
+            for email in User.objects.values_list('email'):
+                user = User.objects.get(email=email)
+        except:
+            flag = False
+
+        self.assertEqual(flag, True)
+
+    def test_allInvitessAreDifferent(self):
+        flag = True
+        try:
+            for invite in User.objects.values_list('invite'):
+                user = User.objects.get(invite=invite)
+        except:
+            flag = False
+
+        self.assertEqual(flag, True)
